6f075c5de0f9ed3ae08245146913f02ee7d2d2c0
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-09 19:50:58 +0530

    [Dipti] added code to print fibonnaci series

---
+++ LogicalProgram_CS/Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram_CS
+{/// <summary>
+/// Template finding fibonacci series
+/// </summary>
+    class Fibonacci
+    {
+            public static void FibonacciSeries()
+            {
+                //local variables
+                int count = 10;
+                int res;
+                Console.WriteLine("Enter two numbers:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                //int num1=0;
+                //int num2=1;
+                for (int i = 2; i <= count; i++)
+                {
+                    res = num1 + num2;
+                    Console.WriteLine("" + res);
+                    num1 = num2;
+                    num2 = res;
+                }
+            }
+
+        
+    }
+}
